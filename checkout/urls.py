0a667d846d03f5adc5153ef061a5813aec1d1ccf
Luciano Martins <zicadopv@gmail.com>
2018-09-12 07:53:50 -0300

    Alterações

---
+++ checkout/urls.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+
+from django.conf.urls import url
+
+from . import views
+
+app_name = 'checkout'
+urlpatterns = [
+    url(
+        r'^carrinho/adicionar/(?P<slug>[\w_-]+)/$', views.create_cartitem,
+        name='create_cartitem'
+    )
+]
